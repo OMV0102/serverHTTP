ab91519d9c65a9e23e831c4b750485f5e28e1ae8
omv0102 <omv98@outlook.com>
2021-12-03 03:52:16 +0700

    Добавил страничку в исходники

--- serverHTTP/serverHTTP.cpp
+++ serverHTTP/serverHTTP.cpp
@@ -2,17 +2,23 @@
 #include <WinSock2.h>
 #include <stdio.h>
 #include <string.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <exception>
 
 #pragma warning(disable:4996)
 #pragma comment(lib, "wsock32.lib") //необходимая библиотека для интерфейса сокетов
+#pragma comment(lib, "Ws2_32.lib")
 
 #define ROOT "public_html"  //корневая папка
 #define HTTP_GET 1
 #define HTTP_HEAD 2
 
+#define SERVER_PORT "3333"
+
 char def_file[] = {"index.html"};
 char error[] = {"HTTP/1.1 404 File not found\n\n"};
-char ok[] = {"HTTP/1.1 200 OK\nServer: CNAI Web Server\nContent-Length: "};
+char ok[] = {"HTTP/1.1 200 OK\nServer: CNAI Web Server\nContent-Length: 0"};
 char ctype[] = {"Content-Type: "};
 char end[] = {"\n\n"};
 
@@ -30,17 +36,18 @@ char types[][20] = {
 char *get_type(char *fname)
 {
 	char *FExt = strrchr(fname, '.'); //последнее вхождение символа '.'
-	for(int i = 0;; i += 2)
-		//находим расширение из списка	types
-		if(!types[i][0] || !strcmp(FExt + 1, types[i]))
-			return types[i + 1];
+	if(FExt != NULL)
+		for(int i = 0;; i += 2)
+			//находим расширение из списка	types
+			if(!types[i][0] || !strcmp(FExt + 1, types[i]))
+				return types[i + 1];
 }
 
 //получаем пакет данных (возвращает размер пакета)
 int recv_data(SOCKET s, char *data)
 {
 	int i, rv = NULL;
-	char tmp[200] = {"\0"};
+	char tmp[500] = {"\0"};
 	do
 	{
 		i = 0;
@@ -104,7 +111,7 @@ void send_data(SOCKET sock, char *file_name, int method)
 
 	if(method == HTTP_GET)//SEEK_SET Начало файла 0
 	{
-		data = (char *) realloc(data, sz); //изменили размер буфера
+		data = (char *) malloc(sz); //изменили размер буфера
 		fread(data, sz, 1, file); //поместили файл в буфер
 	}
 	fclose(file);
@@ -140,45 +147,59 @@ DWORD WINAPI server(SOCKET sock)
 int main()
 {
 	WSAData wsaData; //структура сокета
-	sockaddr_in addr_Sock; //адрес конечной точки
+	struct addrinfo hints;
+	struct addrinfo *addr;
 	SOCKET listen_Sock;
 	WSAStartup(MAKEWORD(2, 2), &wsaData); // инициализация для использования сокетов процессом
 
+	//настраиваем сокет
+	addr = NULL;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET; // семейство протоколов UDP / TCP
+	hints.ai_socktype = SOCK_STREAM; 
+	hints.ai_protocol = IPPROTO_TCP; //номер порта (задается define)
+
+	hints.ai_flags = AI_PASSIVE;
+	if(getaddrinfo("0.0.0.0", SERVER_PORT, &hints, &addr) != 0)
+	{
+		WSACleanup();
+		printf("Getaddrinfo failed");
+		return SOCKET_ERROR;
+	}
+
 	//создание сокета, который связан с клиентом
-	if((listen_Sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+	if((listen_Sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol)) == INVALID_SOCKET)
 	{
 		printf("Unable to create socket\n");
 		WSACleanup();
 		system("pause");
 		return INVALID_SOCKET;
 	}
+	else printf("Socket created\n");
 
-	//настраиваем сокет
-	addr_Sock.sin_family = AF_INET; // семейство протоколов AF_INET
-	addr_Sock.sin_addr.s_addr = INADDR_ANY; //IP-адрес компьютера (смотрим ipconfig IPv4) 
-	addr_Sock.sin_port = htons(2003); //номер порта (2000 + 3 бригада)
-
-	// связываем сокет с данными из addr_Sock
-	if(bind(listen_Sock, (LPSOCKADDR) &addr_Sock, sizeof(sockaddr)) == SOCKET_ERROR)
+	// связываем сокет с данными из addr
+	if(bind(listen_Sock, addr->ai_addr, (int) addr->ai_addrlen) == SOCKET_ERROR)
 	{
 		printf("Unable to bind\n");
 		WSACleanup();
 		system("pause");
 		return SOCKET_ERROR;
 	}
+	else printf("Bind succes\n");
 
 	// слушаем сокет
-	if(listen(listen_Sock, 1) == SOCKET_ERROR)
+	if(listen(listen_Sock, SOMAXCONN) == SOCKET_ERROR)
 	{
 		printf("Unable to listen\n");
 		WSACleanup();
 		system("pause");
 		return SOCKET_ERROR;
 	}
+	else printf("Listen succes\n");
 
 	while(true)
 		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE) &server, (void *) accept(listen_Sock, 0, 0), NULL, NULL);
-	//accept - разрешение на входящее соединение (сокет, указатель на буфер, указатель на длину структуры)
+		//accept - разрешение на входящее соединение (сокет, указатель на буфер, указатель на длину структуры)
 
 	return 0;
 }
